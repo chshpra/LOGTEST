552c5566dcfb0379129eb95f3dbe50e5a0bde2b3
Hengsheng Cai <luke.hengshengcai@gmail.com>
2017-11-21 18:00:17 +0800

    simplified test method

--- Tester/tester.h
+++ Tester/tester.h
@@ -8,13 +8,11 @@ namespace LOGTEST
     public:
         Tester();
 
-        double test_single_thread(Logger& logger, const int &num_logs = 10e6);
-        double test_multi_thread(Logger& logger, const int &num_threads = 10, const int &logs_per_thread = 10e5);
+        void run_performance_test(Logger &logger, const int &num_threads, const int &logs_per_thread);
+        bool run_thread_safety_test(Logger &logger);
         void print_header();
 
     private:
-        void log_loop(Logger &logger, const int &num_iters);
-        bool check_thread_safety(const string &libname);
-        void print_result(const string &lib_name, const int &num_threads, const int &logs_per_thread, const double &secs, bool is_multi_thread_safe);
+        void print_result(const string &lib_name, const int &num_threads, const int &logs_per_thread, const double &secs);
     };
 }
