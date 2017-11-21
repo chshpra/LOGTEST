2bdc66eba129a10959bd872d96469a7fe15c250f
Hengsheng Cai <luke.hengshengcai@gmail.com>
2017-11-21 18:06:27 +0800

    modified spdlog logger

--- Logger/logger_spdlog.cpp
+++ Logger/logger_spdlog.cpp
@@ -2,30 +2,26 @@
 
 namespace LOGTEST
 {
-	Logger_spdlog::Logger_spdlog() 
-	{
-		// create single thread logger
-		spdlogger_st = spdlog::basic_logger_st("spdlogger_st", "./logs/spdlog.txt", false);
-		// create multi-thread logger
-		// the argument must be a power of 2, loggers created from now on will be in async mode
-		spdlog::set_async_mode(8192);
-		spdlogger_mt = spdlog::basic_logger_mt("spdlogger_mt", "./logs/spdlog.txt", false);
-	}
-
 	Logger_spdlog& Logger_spdlog::instance() 
 	{
 		static Logger_spdlog instance;
 		return instance;
 	}
+	
+	Logger_spdlog::Logger_spdlog() 
+	{
+		spdlog::set_async_mode(pow(2, 20)); // the argument must be a power of 2
+		logger = spdlog::basic_logger_mt("logger", "spdlog.txt", true);
+	}
 
 	void Logger_spdlog::log(const string &msg) 
 	{
-		spdlogger_st->info(msg);
+		logger->info(msg);
 	}
 
-	void Logger_spdlog::log_mt(const string &msg) 
+	void Logger_spdlog::flush()
 	{
-		spdlogger_mt->info(msg);
+		logger->flush();
 	}
 
 	string Logger_spdlog::get_libname()
